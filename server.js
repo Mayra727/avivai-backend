f591819f0bd628d8afd893fd5a6ae71ba3eed3e0
Mayra Caroline <mayracarol727@gmail.com>
2026-03-08 23:23:11 -0300

    fix railway port

--- server.js
+++ server.js
@@ -336,6 +336,8 @@ app.get("/my-courses/:userId", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("🚀 Servidor rodando na porta 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("🚀 Servidor rodando na porta", PORT);
 });
