3d6a7f41cae1a0097a27bebc3b4c67de731fb5a3
Mayra Caroline <mayracarol727@gmail.com>
2026-05-05 21:55:15 -0300

    fix: correct cloudinary env names

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ import multer from "multer";
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 console.log("🔥 CLOUD:", process.env.CLOUDINARY_CLOUD_NAME);
